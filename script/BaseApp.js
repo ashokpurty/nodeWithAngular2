fbdea16c1dc36833130e1531b441851d4f9f281a
ashok purty <ashok.purty@wk.chd>
2018-03-17 16:57:54 +0530

    Added angulr grid view

--- script/BaseApp.js
+++ script/BaseApp.js
@@ -1,16 +1,91 @@
-var app = angular.module("myApp", []); 
+var app = angular.module("myApp", [
+    'ui.grid',
+    'ui.grid.pagination',
+    'ui.grid.selection',
+    'ui.grid.cellNav',
+    'ui.grid.expandable',
+    'ui.grid.edit',
+    'ui.grid.rowEdit',
+    'ui.grid.saveState',
+    'ui.grid.resizeColumns',
+    'ui.grid.pinning',
+    'ui.grid.moveColumns',
+    'ui.grid.exporter',
+    'ui.grid.infiniteScroll',
+    'ui.grid.importer',
+    'ui.grid.grouping'
+]); 
 
-app.controller('myCtrl', function($scope, $http) {
-    $scope.firstName = "Doe";
+app.controller('myCtrl',['$scope', '$http', '$log', '$timeout', 'uiGridConstants', '$q', '$interval', function($scope, $http, $log, $timeout, uiGridConstants, $q, $interval) {
     $scope.employeeCode = "";
     $scope.name = "";
     $scope.age = "";
     $scope.department = "";
     $scope.designation = "";
     $scope.designation = "";
-    
+    debugger;
+
+    var resultData = [];
+    $scope.employeeGridData = {
+        data: resultData,
+        columnDefs: [
+            {
+                name: 'employeeCode',
+                displayName: 'Employee Code',
+                enableSorting: true,
+                enableRowSelection: true,
+                cellTemplate: 'editData.html',
+                width: '*',
+                cellTemplate: '<div class="ui-grid-cell-contents" >' +
+                    '<button value="Edit" ng-if="!row.inlineEdit.isEditModeOn" ng-click="row.inlineEdit.enterEditMode($event)">Delete</button>' +
+                    '<button value="Edit" ng-if="!row.inlineEdit.isEditModeOn" ng-click="row.inlineEdit.enterEditMode($event)">Edit</button>' +
+                    '<button value="Edit" ng-if="row.inlineEdit.isEditModeOn" ng-click="row.inlineEdit.saveEdit($event)">Update</button>' +
+                    '<button value="Edit" ng-if="row.inlineEdit.isEditModeOn" ng-click="row.inlineEdit.cancelEdit($event)">Cancel</button>' +
+                    '</div>',
+                    enableCellEdit: false,
+                    enableFiltering:false,
+                    showSortMenu : false,
+                    enableColumnMenu: false,
+            },
+            {
+                name: 'name',
+                displayName: 'Employee Name',
+                enableSorting: true,
+                width: '*',
+                enableCellEdit: true,
+                cellTemplate: '<div class="ui-grid-cell-contents"><div ng-class="{\'viewr-dirty\' : row.inlineEdit.entity[col.field].isValueChanged }">{{row.entity[col.field]}}</div></div>'
+            },
+            {
+                name: 'age',
+                displayName: 'Age',
+                enableSorting: false,
+                width: '*',
+                enableCellEdit: true,
+                cellTemplate: '<div class="ui-grid-cell-contents"><div ng-class="{\'viewr-dirty\' : row.inlineEdit.entity[col.field].isValueChanged }">{{row.entity[col.field]}}</div></div>'
+            },
+            {
+                name: 'department',
+                displayName: 'Department',
+                enableSorting: false,
+                width: '*',
+                enableCellEdit: true,
+                cellTemplate: '<div class="ui-grid-cell-contents"><div ng-class="{\'viewr-dirty\' : row.inlineEdit.entity[col.field].isValueChanged }">{{row.entity[col.field]}}</div></div>'
+            },
+            {
+                name: 'designation',
+                displayName: 'Designation',
+                enableSorting: false,
+                width: '*',
+                enableCellEdit: true,
+                cellTemplate: '<div class="ui-grid-cell-contents"><div ng-class="{\'viewr-dirty\' : row.inlineEdit.entity[col.field].isValueChanged }">{{row.entity[col.field]}}</div></div>'
+            }
+
+      ]
+    }
+
     $http.get("http://localhost:8585/employee").then(function (response) {
-        $scope.employee = response.data;
+        debugger;
+        $scope.employeeGridData = resultData = response.data  ;
     });
 	
 	$scope.addEmployee = function () {
@@ -29,5 +104,10 @@ app.controller('myCtrl', function($scope, $http) {
             if (response.data)
                 $scope.msg = "Put Data Method Executed Successfully!";
             });
-	};
-});
+    };
+    
+    $scope.editEmployee = function(){
+        debugger;
+        alert('Hello');
+    };
+}]);
